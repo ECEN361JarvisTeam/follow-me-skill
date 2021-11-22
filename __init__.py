d9f7283feec08dce579d305b0421c88a2c903162
ECEN361JarvisTeam <noahkfish@gmail.com>
2021-11-22 13:42:36 -0700

    Added functions to control pan/tilt movement.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
 from mycroft import MycroftSkill, intent_file_handler
+import face_recog_functions
+import cv2 as cv
 
 
 class FollowMe(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+	m = maestro.Controller("/dev/ttyACM0")
 
     @intent_file_handler('me.follow.intent')
     def handle_me_follow(self, message):
-        self.speak_dialog('me.follow')
-
+	m = maestro.controller("/dev/ttyACM0")
+        #person = self.get_response('')
+	panTiltTrackLoop(m, cv.VideoCapture(0))
+	
 
 def create_skill():
     return FollowMe()
