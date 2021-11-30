ca871a10aea5e9fffaff56d3cb90961d8b00fc78
ECEN361JarvisTeam <noahkfish@gmail.com>
2021-11-29 17:23:33 -0700

    Added servo following code and ability for following loop to be exited.

--- __init__.py
+++ __init__.py
@@ -1,19 +1,36 @@
 from mycroft import MycroftSkill, intent_file_handler
-import face_recog_functions
+import face_recog_functions as frf
+import os
+import threading
+import maestro
 import cv2 as cv
 
 
 class FollowMe(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-	m = maestro.Controller("/dev/ttyACM0")
+        self.stopLoop = False
+        self.stopLoopLock = threading.Lock()
 
     @intent_file_handler('me.follow.intent')
     def handle_me_follow(self, message):
-	m = maestro.controller("/dev/ttyACM0")
-        #person = self.get_response('')
-	panTiltTrackLoop(m, cv.VideoCapture(0))
-	
+        followName = message.data.get("name")
+        
+        if not followName == "me":
+            self.speak_dialog("me.follow.wait")
+        
+        videoInput = cv.VideoCapture(0)
+        m = maestro.Controller("/dev/ttyACM0")
+        frf.panTiltTrackLoop(m, videoInput, frf.mycroftFacesPath, self, followName)
+        self.speak_dialog("me.follow.stop")
+        videoInput.release()
+
+    def converse(self, message):
+        if message.data["utterances"][0] == "stop":
+            self.stopLoopLock.acquire()
+            self.stopLoop = True
+            self.stopLoopLock.release()
+            return True
 
 def create_skill():
     return FollowMe()
